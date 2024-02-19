dc0473c85f7e330ae5e93aa800b1a7618b968f22
SuvhanJ737 <148375754+SuvhanJ737@users.noreply.github.com>
2024-02-19 21:32:31 +0200

    Add files via upload

---
+++ Calculator/app.js
@@ -0,0 +1,32 @@
+let inoutBox = document.getElementById('inputBox')
+let button = document.querySelectorAll('button')
+
+let string = ''
+
+button.forEach(element => {
+
+element.addEventListener('click', (b)=>{
+    if (b.target.innerText == '='){
+        string = String(eval(string))
+        inputBox.value = string;
+    }
+    else if (b.target.innerText == 'AC'){
+        string = ''
+        inputBox.value = string;
+    }
+    else if(b.target.innerTexxt == 'DEL') {
+        string = string.substring(0,string.length-1)
+        inputBox.value = string;
+    }
+    else if (b.target.id == 'plusMinus'){
+        string = String(-eval(string))
+        inputBox.value = string;
+    }
+    else {
+        string += b.target.innerText
+        inputBox.value = string
+    }
+})
+})
+
+
